d7046bb7caebf9a585de6030a37140bf0de6e027
eersels3764 <ivo.eersels@idewe.be>
2021-08-06 17:08:08 +0200

    Message Converter Configuration -------------------------------
    We want to send a TextMessage with a JSON payload of the object we are working with. So, we need to setup a MessageConverter to do that.
    
    	@Configuration
    	public class JmsConfig {
    
    		@Bean
    		public MessageConverter messageConverter() {
    			MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter(); // Spring will provide an objectMapper and convert any message that we send into a JMS TextMessage and convert the object we provide to a JSON payload
    			converter.setTargetType(MessageType.TEXT);
    			converter.setTypeIdPropertyName("_type");                                           // In this property we will specify the class name as what Spring needs to deserialize the (JSON) message payload to a Java payload
    
    			return converter;
    		}
    	}

---
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String MY_QUEUE_NAME = "my-hello-world";
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter(); // Spring will provide an objectMapper
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");                                           // In this property we will specify the class name as what Spring needs to deserialize the (JSON) message payload
+
+        return converter;
+    }
+}
